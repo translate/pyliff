a9c472be8250f3c1ab12c80af6a23bb6afe617f2
Ryan Northey <ryan@space23.net>
2016-01-25 20:13:21 +0000

    Add attribution

--- setup.py
+++ setup.py
@@ -1,21 +1,10 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2013 Translate House
+# Copyright (C) Translate House and contributors.
 #
-# This file is part of pootle_fs.
-#
-# This software is free software; you can redistribute it and/or modify it under
-# the terms of the GNU General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
-# version.
-#
-# This software is distributed in the hope that it will be useful, but WITHOUT ANY
-# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-# A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License along with
-# this software; if not, see <http://www.gnu.org/licenses/>.
+# This file is a part of the PyLIFF project. It is distributed under the GPL3
+# or later license. See the LICENSE file for a copy of the license and the
+# AUTHORS file for copyright and authorship information.
 
 """Python XLIFF object
 """

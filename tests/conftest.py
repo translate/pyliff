6356b453421f75b28f0c11be0d46446bf8d919f2
Ryan Northey <ryan@space23.net>
2016-01-26 11:38:56 +0000

    Separate test fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,8 @@
+from fixtures import (
+    files_xliff, file_units_xliff, file_groups_xliff,
+    file_notes_xliff)
+
+
+__all__ = (
+    "files_xliff", "file_units_xliff", "file_groups_xliff",
+    "file_notes_xliff")

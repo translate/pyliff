1e4e46d022e614290984f8d6ff5fc9bd36c1b388
Ryan Northey <ryan@space23.net>
2016-01-27 18:20:50 +0000

    Cleanup: remove unused code

--- pyliff/__init__.py
+++ pyliff/__init__.py
@@ -6,35 +6,7 @@
 # or later license. See the LICENSE file for a copy of the license and the
 # AUTHORS file for copyright and authorship information.
 
-
-from lxml import etree
-
-from .liff import LanguageInterchangeFormat
+from .liff import LanguageInterchangeFormat as PyLIFF
 
 
-class PyLIFF(LanguageInterchangeFormat):
-
-    @property
-    def xml(self):
-        return etree.fromstring(
-            self.src.encode(),
-            parser=etree.XMLParser(
-                ns_clean=True,
-                recover=True,
-                encoding='utf-8'))
-
-    @property
-    def namespaces(self):
-        pass
-
-    @property
-    def root_path(self):
-        return "/xliff:xliff"
-
-    def serialize(self):
-        return etree.tostring(self.xml)
-
-    def xpath(self, path):
-        return self.xml.xpath(
-            self.root_path + path,
-            namespaces={'xliff': 'urn:oasis:names:tc:xliff:document:2.0'})
+__all__ = ("PyLIFF", )
